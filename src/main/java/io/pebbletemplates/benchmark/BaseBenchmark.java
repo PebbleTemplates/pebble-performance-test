1aabfef598164eeea340b209ef402b2d942a8d95
Eric Bussieres <erbussieres@gmail.com>
2018-07-10 22:39:29 -0400

    Update time for each iteration

--- src/main/java/io/pebbletemplates/benchmark/BaseBenchmark.java
+++ src/main/java/io/pebbletemplates/benchmark/BaseBenchmark.java
@@ -1,7 +1,9 @@
 package io.pebbletemplates.benchmark;
 
 import io.pebbletemplates.benchmark.model.Stock;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
@@ -11,13 +13,9 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 @Fork(5)
-@Warmup(iterations = 5)
-@Measurement(iterations = 10)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 10, time = 1)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
